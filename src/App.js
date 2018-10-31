45cac7c3d7267bcdfb4ef013873dff64d416e4c4
liujie <jackxuechen@163.com>
2018-10-31 16:23:44 +0800

    app修改

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {Button, Container, Content, Footer, FooterTab, Text} from 'native-base';
+import React, { Component } from 'react';
+import { Button, Container, Content, Footer, FooterTab, Text } from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
 import I18n from './locales/i18n'
 
-export default class FooterTabsIconTextExample extends Component {
+export default class App extends Component {
 
     render() {
         return (
@@ -12,20 +12,24 @@ export default class FooterTabsIconTextExample extends Component {
                 <Footer>
                     <FooterTab>
                         <Button vertical>
-                            <Icon name="facebook-square" />
-                            <Text>Apps</Text>
-                        </Button>
-                        <Button vertical>
-                            <Icon name="test" size={30} color="#4F8EF7"/>
-                            <Text>Camera</Text>
-                        </Button>
-                        <Button vertical active>
-                            <Icon active name="navigate" />
-                            <Text>{I18n.t()}</Text>
-                        </Button>
-                        <Button vertical>
-                            <Icon name="person" />
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
                             <Text>{I18n.t('blog')}</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Text>{I18n.t('project')}</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Text>{I18n.t('official_accounts')}</Text>
+                        </Button>
+                        <Button vertical active size={30}>
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Text>{I18n.t('knowledge_hierarchy')}</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Text>{I18n.t('personal_center')}</Text>
                         </Button>
                     </FooterTab>
                 </Footer>
--- src/locales/zh.js
+++ src/locales/zh.js
@@ -2,6 +2,6 @@ export default{
     blog:'博客',
     project:'项目',
     official_accounts:'公众号',
-    knowledge_hierarchy:'知识体系',
+    knowledge_hierarchy:'体系',
     personal_center:'我的',
 }
