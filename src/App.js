2dd4724ae6b8790bdf9583540b7e794faf50c15f
liujie <jackxuechen@163.com>
2018-10-31 16:40:40 +0800

    首页

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Button, Container, Content, Footer, FooterTab, Text } from 'native-base';
+import React, {Component} from 'react';
+import {Button, Container, Content, Footer, FooterTab, Text, Header, Left, Right, Body, Title} from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
 import I18n from './locales/i18n'
 
@@ -8,27 +8,34 @@ export default class App extends Component {
     render() {
         return (
             <Container>
-                <Content />
+                <Header>
+                    <Left/>
+                    <Body>
+                    <Title>test</Title>
+                    </Body>
+                    <Right/>
+                </Header>
+                <Content/>
                 <Footer>
                     <FooterTab>
                         <Button vertical>
-                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Icon name="md-musical-notes" active size={30} color="#4F8EF7"/>
                             <Text>{I18n.t('blog')}</Text>
                         </Button>
                         <Button vertical>
-                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7"/>
                             <Text>{I18n.t('project')}</Text>
                         </Button>
                         <Button vertical>
-                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7"/>
                             <Text>{I18n.t('official_accounts')}</Text>
                         </Button>
-                        <Button vertical active size={30}>
-                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                        <Button vertical size={30}>
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7"/>
                             <Text>{I18n.t('knowledge_hierarchy')}</Text>
                         </Button>
                         <Button vertical>
-                            <Icon name="md-musical-notes" size={30} color="#4F8EF7" />
+                            <Icon name="md-musical-notes" size={30} color="#4F8EF7"/>
                             <Text>{I18n.t('personal_center')}</Text>
                         </Button>
                     </FooterTab>
