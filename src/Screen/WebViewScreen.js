abe38abf6ef7e1ba935d39dadc52cdca57cf3eef
liujie <jackxuechen@163.com>
2018-11-02 17:18:16 +0800

    webScreen 添加loading

--- src/Screen/WebViewScreen.js
+++ src/Screen/WebViewScreen.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import { WebView, ActivityIndicator, View } from 'react-native';
+import { color } from '../values/color';
 
 export default class WebViewScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true
+        }
+    }
     render() {
         return (
-            <WebView
-                source={{ uri: this.props.navigation.getParam('url','')}}
-            />
+            <View style={{ flex: 1,backgroundColor:color.color_ffffff }}>
+                {this.state.isLoading ? <ActivityIndicator size='large' color={color.color_5b71f9} style={{backgroundColor:color.color_ffffff }} /> : <View />}
+                <WebView
+                    onLoadStart={() => {
+                        this.setState({
+                            isLoading: true
+                        })
+                    }}
+                    onLoadEnd={
+                        () => {
+                            this.setState({
+                                isLoading: false
+                            })
+                        }
+                    }
+                    source={{ uri: this.props.navigation.getParam('url', '') }}
+                />
+            </View>
+
         );
     }
 }
--- src/values/color.js
+++ src/values/color.js
@@ -3,4 +3,5 @@ export const color = {
     color_f5f5f7: "#f5f5f7",
     color_5b71f9: "#5b71f9",
     color_333333: "#333333",
+    color_ffffff: "#ffffff",
 } 
