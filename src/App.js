81e3e6764f53433b7c8fa2542f0cd5168969b861
liujie <jackxuechen@163.com>
2018-11-03 13:38:23 +0800

    调整目录结构

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import WebViewScreen from './Screen/WebViewScreen';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator } from 'react-navigation';
-import BlogScreen from "./Screen/BlogScreen";
-import ProjectScreen from "./Screen/ProjectScreen";
+import MainScreen from './Screen/MainScreen';
+
 
 
 const RootStack = StackNavigator(
     {
         Main: {
-            screen: createBottomTabNavigator(
-                {
-                    Blog: BlogScreen,
-                    Project: ProjectScreen,
-                },
-                {
-                    navigationOptions: ({ navigation }) => ({
-                        tabBarIcon: ({ focused, horizontal, tintColor }) => {
-                            const { routeName } = navigation.state;
-                            let iconName;
-                            if (routeName === 'Blog') {
-                                iconName = `ios-bug${focused ? '' : ''}`;
-                            } else if (routeName === 'Project') {
-                                iconName = `ios-bug${focused ? '' : ''}`;
-                            }
-                            return <Icon name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
-                        },
-                    }),
-                    tabBarOptions: {
-                        activeTintColor: 'tomato',
-                        inactiveTintColor: 'gray',
-                    },
-                },
-            ),
+            screen: MainScreen
         },
         Web: {
             screen: WebViewScreen,
---
+++ src/Screen/MainScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from 'react-navigation';
+import BlogScreen from "./BlogScreen";
+import ProjectScreen from "./ProjectScreen";
+
+export default MainScreen = createBottomTabNavigator(
+    {
+        Blog: BlogScreen,
+        Project: ProjectScreen,
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Blog') {
+                    iconName = `ios-bug${focused ? '' : ''}`;
+                } else if (routeName === 'Project') {
+                    iconName = `ios-bug${focused ? '' : ''}`;
+                }
+                return <Icon name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    },
+)
