f141e762723395a2cc66d11f29df0be2bb2216a2
liujie <jackxuechen@163.com>
2018-11-06 11:19:50 +0800

    添加日志工具类

--- src/api/ApiUrl.js
+++ src/api/ApiUrl.js
@@ -1,19 +1,63 @@
+import Storage from "../dao/Storage";
+import L from "../util/L";
+
 export const baseUrl = 'http://www.wanandroid.com/'
 
 export function apiGet(request) {
     return new Promise(function (resolve, reject) {
-        let url=`${baseUrl}${request}`
-        console.info('apiGet\n','request:\n',`\turl:${url}`)
+        let url = `${baseUrl}${request}`
+        L.v('apiGet\n', 'request:\n', `\turl:${url}`)
         fetch(url)
             .then((Response) => Response.json())
             .then(responseJson => {
-                console.info('response:\n\t', responseJson)
-                console.info('---------------------------------------------')
+                L.v('response:\n\t', responseJson)
+                L.v('---------------------------------------------')
                 resolve(responseJson)
             })
             .catch(error => {
-                console.error('error:\n\t', error.toString())
-                console.info('---------------------------------------------')
+                L.e('error:\n\t', error.toString())
+                L.v('---------------------------------------------')
+                reject(error)
+            })
+    });
+};
+export function apiPost(request, param = null) {
+    return new Promise(function (resolve, reject) {
+        let url = `${baseUrl}${request}`
+        L.v('apiGet\n', 'request:\n', `\turl:${url}`)
+        let requsetConfig = {
+            method: 'POST',
+        }
+
+        if (param != null) {
+            let formData = new FormData()
+            for (key in param) {
+                formData.append(key, param[key])
+            }
+            requsetConfig['body'] = formData
+        }
+        let cookie = ''
+        requsetConfig['headers'] = {
+            'Cookie': cookie
+        }
+        L.v('param:\n', `\tparam:${requsetConfig}`)
+        fetch(url, requsetConfig)
+            .then((res) => {
+                if ((res.url.indexOf('user/login') != -1 || res.url.indexOf('user/register')) != -1 &&
+                    res.headers.map.hasOwnProperty('set-cookie')) {
+                    const cookie = res.headers.map['set-cookie'][0]
+                    Storage.setItem('cookie', cookie)
+                }
+                return res.json()
+            })
+            .then(responseJson => {
+                L.v('response:\n\t', responseJson)
+                L.v('---------------------------------------------')
+                resolve(responseJson)
+            })
+            .catch(error => {
+                L.e('error:\n\t', error.toString())
+                L.v('---------------------------------------------')
                 reject(error)
             })
     });
---
+++ src/util/L.js
@@ -0,0 +1,13 @@
+export default class L {
+    static debug = true
+    static v(message, ...optionalParam) {
+        if (this.debug == true) {
+            console.info(message, optionalParam)
+        }
+    }
+    static e(message, ...optionalParam) {
+        if (this.debug == true) {
+            console.error(message, optionalParam)
+        }
+    }
+}
