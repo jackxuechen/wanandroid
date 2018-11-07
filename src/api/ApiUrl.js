b1cfccd9ad460a311237b9b860adc9fb992cac1b
liujie <jackxuechen@163.com>
2018-11-07 16:27:27 +0800

    ios环境配置

--- src/api/ApiUrl.js
+++ src/api/ApiUrl.js
@@ -30,7 +30,7 @@ export async function apiRequest(request, method = 'GET', param = null) {
         if ((res.url.indexOf('user/login') != -1 || res.url.indexOf('user/register')) != -1 &&
             res.headers.map.hasOwnProperty('set-cookie')) {
             const cookie = res.headers.map['set-cookie']
-            await AsyncStorage.setItem('cookie', cookie)
+            await AsyncStorage.setItem('cookie', cookie.toString())
         }
         let resJson = await res.json()
         L.v(`网络请求 url:${url}`, '返回结果 res:', res)
